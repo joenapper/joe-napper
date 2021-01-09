6ce6b0a73a1cfa1eb045e2767181ae4fcbe3cfee
Joe Napper <joe.napper@cmisukltd.onmicrosoft.com>
2021-01-09 22:53:36 +0000

    Used react-helmet to improve SEO

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // Router
 import { BrowserRouter } from "react-router-dom";
+// Helmet
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
